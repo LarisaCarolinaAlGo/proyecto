af6b65915125ef22fa75996f8b8b564249d5c9f0
LarisaCarolinaAlGo <larialgo.caro@gmail.com>
2020-03-12 16:38:38 -0600

    hoy es doce de marzo

--- marcadores.js
+++ marcadores.js
@@ -1,3 +1,13 @@
-function big(){
-  alert("hola")
+function big(x){
+  x.style.fontFamily = "cursive";
+  var size = document.getElementById("crayolaSize");
+  size.style.height = "10px"
+  size.style.width = "10px"
+}
+function normal(y){
+ y.style.fontFamily = "Helvetica"
+ var size= document.getElementById("normalSize");
+ size.style.height = "255.53px"
+ size.style.width = "388.19px"
+
 }

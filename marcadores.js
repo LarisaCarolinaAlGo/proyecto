e5880905825a67ec8868624e2899c6315a54b56a
LarisaCarolinaAlGo <larialgo.caro@gmail.com>
2020-03-31 17:30:48 -0600

    31 de marzo

--- marcadores.js
+++ marcadores.js
@@ -1,16 +1,14 @@
 function big(x){
-  x.style.fontFamily = "cursive";
-  x.style.height = "64px";
-  x.style.width = "64px";
 }
 function normal(y){
  y.style.fontFamily = "Helvetica"
 
 }
  function nuevo(img){
-   img.style.width= "1000px"
+
  }
 function img_normal(imgN){
-  imgN.style.width= "410px"
+  imgN.style.width= "400px"
   imgN.style.height = "260.98px"
 }
+img.style.width= "1000px"

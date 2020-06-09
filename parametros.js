a59d676d39cccfb56d1d14ac6fb97d3ea7ef7e3a
LarisaCarolinaAlGo <larialgo.caro@gmail.com>
2020-06-09 17:41:39 -0500

    Voy en los Stabilo

---
+++ parametros.js
@@ -0,0 +1,12 @@
+function dobleNumero(numero) {
+  let doble = numero *2
+  return doble;
+}
+
+console.log("el doble de tu número es", dobleNumero(3));
+
+function cubo(numero) {
+  let igual = (numero*numero) * numero
+  return igual;
+}
+console.log("Tu numero al cubo es igual a ", cubo(5));
